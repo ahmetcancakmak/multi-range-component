a7f542f4c31a7e392aa3c8a2d1653ccc79c053e5
ahmetcancakmak <56587103+ahmetcancakmak@users.noreply.github.com>
2022-08-16 17:27:36 +0300

    multi-range v2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
-  <MultiRangeComponent :min="min" :max="max" :step="step"/>
+  <div class="h-screen flex justify-center items-center">
+    <div class="relative max-w-xl w-full">
+      <div>
+        <MultiRangeComponent
+          :min="min"
+          :max="max"
+          :step="step"
+          v-model:label="label"
+        />
+        <div class="flex justify-between items-center py-5">
+          <div>
+            <input
+              type="text"
+              v-model="label[0]"
+              class="px-3 py-2 border border-gray-200 rounded w-24 text-center"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              v-model="label[1]"
+              class="px-3 py-2 border border-gray-200 rounded w-24 text-center"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,11 +39,12 @@ export default {
   },
   data() {
     return {
-      min:0,
-      max:1,
-      step:0.1
-    }
-  }
+      min: 0,
+      max: 1000,
+      step: 0.1,
+      label: [0, 1],
+    };
+  },
 };
 </script>
 
--- src/components/MultiRangeComponent.vue
+++ src/components/MultiRangeComponent.vue
@@ -1,37 +1,78 @@
 <template>
-  <div class="h-screen flex justify-center items-center">
-    <div class="relative max-w-xl w-full">
-      <div>
-          <input type="range"
-                 :step="step"
-                 :min="min" 
-                 :max="max"
-                 :change="minLimitSetter()"
-                 v-model="rangeValue[0]"
-                 class="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer">
-          <input type="range"
-                 :step="step"
-                 :min="min" 
-                 :max="max"
-                 :change="maxLimitSetter()"
-                 v-model="rangeValue[1]"
-                 class="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer">
-          <div class="relative z-10 h-2">
-            <div class="absolute z-10 left-0 right-0 bottom-0 top-0 rounded-md bg-gray-200"></div>
-            <div class="absolute z-20 top-0 bottom-0 rounded-md bg-green-300" :style="'right:'+rangeThumb[1]+'%; left:'+rangeThumb[0]+'%'"></div>
-            <div class="absolute z-30 w-6 h-6 top-0 left-0 bg-green-300 rounded-full -mt-2 -ml-1" :style="'left: '+rangeThumb[0]+'%'"></div>
-            <div class="absolute z-30 w-6 h-6 top-0 right-0 bg-green-300 rounded-full -mt-2 -mr-3" :style="'right: '+rangeThumb[1]+'%'"></div>
-          </div>
-      </div>
-      <div class="flex justify-between items-center py-5">
-        <div>
-          <input type="text" v-model="rangeValue[0]" class="px-3 py-2 border border-gray-200 rounded w-24 text-center">
-        </div>
-        <div>
-          <input type="text" v-model="rangeValue[1]" class="px-3 py-2 border border-gray-200 rounded w-24 text-center">
-        </div>
-      </div>
-    </div>
+  <input
+    type="range"
+    :step="step"
+    :min="min"
+    :max="max"
+    :change="minLimitSetter()"
+    v-model="rangeValue[0]"
+    class="
+      absolute
+      pointer-events-none
+      appearance-none
+      z-20
+      h-2
+      w-full
+      opacity-0
+      cursor-pointer
+    "
+  />
+  <input
+    type="range"
+    :step="step"
+    :min="min"
+    :max="max"
+    :change="maxLimitSetter()"
+    v-model="rangeValue[1]"
+    class="
+      absolute
+      pointer-events-none
+      appearance-none
+      z-20
+      h-2
+      w-full
+      opacity-0
+      cursor-pointer
+    "
+  />
+  <div class="relative z-10 h-2">
+    <div
+      class="absolute z-10 left-0 right-0 bottom-0 top-0 rounded-md bg-gray-200"
+    ></div>
+    <div
+      class="absolute z-20 top-0 bottom-0 rounded-md bg-green-300"
+      :style="'right:' + rangeThumb[1] + '%; left:' + rangeThumb[0] + '%'"
+    ></div>
+    <div
+      class="
+        absolute
+        z-30
+        w-6
+        h-6
+        top-0
+        left-0
+        bg-green-300
+        rounded-full
+        -mt-2
+        -ml-1
+      "
+      :style="'left: ' + rangeThumb[0] + '%'"
+    ></div>
+    <div
+      class="
+        absolute
+        z-30
+        w-6
+        h-6
+        top-0
+        right-0
+        bg-green-300
+        rounded-full
+        -mt-2
+        -mr-3
+      "
+      :style="'right: ' + rangeThumb[1] + '%'"
+    ></div>
   </div>
 </template>
 
@@ -42,30 +83,42 @@ export default {
     min: Number,
     max: Number,
     step: Number,
+    label: Array,
   },
+  emits: ["update:label"],
   data() {
     return {
-      rangeValue: new Array,
-      rangeThumb: [0,0]
+      rangeValue: new Array(),
+      rangeThumb: [0, 0],
     };
   },
   methods: {
     minLimitSetter() {
-      if((((this.rangeValue[0] - this.min) / (this.max - this.min)) * 100) > 0){
-        this.rangeValue[0] = Math.min(this.rangeValue[0], this.rangeValue[1] - this.step);
-        this.rangeThumb[0] = ((this.rangeValue[0] - this.min) / (this.max - this.min)) * 100;
-      }else{
+      if (((this.rangeValue[0] - this.min) / (this.max - this.min)) * 100 > 0) {
+        this.rangeValue[0] = Math.min(
+          this.rangeValue[0],
+          this.rangeValue[1] - this.step
+        );
+        this.rangeThumb[0] =
+          ((this.rangeValue[0] - this.min) / (this.max - this.min)) * 100;
+      } else {
         this.rangeThumb[0] = 0;
       }
+      this.$emit("update:label", this.rangeValue);
     },
     maxLimitSetter() {
-        this.rangeValue[1] = Math.max(this.rangeValue[1], this.rangeValue[0] + this.step);
-        this.rangeThumb[1] = 100 - (((this.rangeValue[1] - this.min) / (this.max - this.min)) * 100);
-    }
+      this.rangeValue[1] = Math.max(
+        this.rangeValue[1],
+        this.rangeValue[0] + this.step
+      );
+      this.rangeThumb[1] =
+        100 - ((this.rangeValue[1] - this.min) / (this.max - this.min)) * 100;
+      this.$emit("update:label", this.rangeValue);
+    },
   },
-  created(){
-    this.rangeValue[0]= this.min;
-    this.rangeValue[1]= this.max;
+  created() {
+    this.rangeValue[0] = this.min;
+    this.rangeValue[1] = this.max;
   },
 };
 </script>
