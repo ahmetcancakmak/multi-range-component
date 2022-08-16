df2d0adc62986d1e029d35f1324eaa42942870cf
ahmetcancakmak <56587103+ahmetcancakmak@users.noreply.github.com>
2022-08-16 16:40:20 +0300

    multi-range-component v1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MultiRangeComponent :min="min" :max="max" :step="step"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MultiRangeComponent from "./components/MultiRangeComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    MultiRangeComponent,
+  },
+  data() {
+    return {
+      min:0,
+      max:1,
+      step:0.1
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/MultiRangeComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <div class="relative max-w-xl w-full">
+      <div>
+          <input type="range"
+                 :step="step"
+                 :min="min" 
+                 :max="max"
+                 :change="minLimitSetter()"
+                 v-model="rangeValue[0]"
+                 class="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer">
+          <input type="range"
+                 :step="step"
+                 :min="min" 
+                 :max="max"
+                 :change="maxLimitSetter()"
+                 v-model="rangeValue[1]"
+                 class="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer">
+          <div class="relative z-10 h-2">
+            <div class="absolute z-10 left-0 right-0 bottom-0 top-0 rounded-md bg-gray-200"></div>
+            <div class="absolute z-20 top-0 bottom-0 rounded-md bg-green-300" :style="'right:'+rangeThumb[1]+'%; left:'+rangeThumb[0]+'%'"></div>
+            <div class="absolute z-30 w-6 h-6 top-0 left-0 bg-green-300 rounded-full -mt-2 -ml-1" :style="'left: '+rangeThumb[0]+'%'"></div>
+            <div class="absolute z-30 w-6 h-6 top-0 right-0 bg-green-300 rounded-full -mt-2 -mr-3" :style="'right: '+rangeThumb[1]+'%'"></div>
+          </div>
+      </div>
+      <div class="flex justify-between items-center py-5">
+        <div>
+          <input type="text" v-model="rangeValue[0]" class="px-3 py-2 border border-gray-200 rounded w-24 text-center">
+        </div>
+        <div>
+          <input type="text" v-model="rangeValue[1]" class="px-3 py-2 border border-gray-200 rounded w-24 text-center">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MutliRangeComponent",
+  props: {
+    min: Number,
+    max: Number,
+    step: Number,
+  },
+  data() {
+    return {
+      rangeValue: new Array,
+      rangeThumb: [0,0]
+    };
+  },
+  methods: {
+    minLimitSetter() {
+      if((((this.rangeValue[0] - this.min) / (this.max - this.min)) * 100) > 0){
+        this.rangeValue[0] = Math.min(this.rangeValue[0], this.rangeValue[1] - this.step);
+        this.rangeThumb[0] = ((this.rangeValue[0] - this.min) / (this.max - this.min)) * 100;
+      }else{
+        this.rangeThumb[0] = 0;
+      }
+    },
+    maxLimitSetter() {
+        this.rangeValue[1] = Math.max(this.rangeValue[1], this.rangeValue[0] + this.step);
+        this.rangeThumb[1] = 100 - (((this.rangeValue[1] - this.min) / (this.max - this.min)) * 100);
+    }
+  },
+  created(){
+    this.rangeValue[0]= this.min;
+    this.rangeValue[1]= this.max;
+  },
+};
+</script>
+
+<style scoped>
+input[type="range"]::-webkit-slider-thumb {
+  pointer-events: all;
+  width: 24px;
+  height: 24px;
+  -webkit-appearance: none;
+}
+</style>
